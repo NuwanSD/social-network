d1e24fb500182337ede5ce38dd3457dadc5ba602
NuwanSD <sagaradnuwan@gmail.com>
2025-06-05 17:21:14 +0530

    End of section 15

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -13,17 +13,19 @@ import {
 
 import { Link } from "react-router";
 import { formatDate } from "../../../lib/util/util";
+import AvatarPopover from "../../../app/shared/components/AvatarPopover";
 
 type Props = {
   activity: Activity;
 };
 
 export default function ActivityCard({ activity }: Props) {
-  const isHost = false;
-  const isGoing = false;
-  const isCancelled = false;
-  const label = isHost ? "You are hosting" : "You are going";
-  const color = isHost ? "secondary" : isGoing ? "warning" : "default";
+  const label = activity ? "You are hosting" : "You are going";
+  const color = activity.isHost
+    ? "secondary"
+    : activity.isGoing
+    ? "warning"
+    : "default";
 
   return (
     <Card elevation={3} sx={{ borderRadius: 3 }}>
@@ -37,15 +39,23 @@ export default function ActivityCard({ activity }: Props) {
           }}
           subheader={
             <>
-              Hosted by <Link to={`/profiles/bob`}>Bob</Link>
+              Hosted by{" "}
+              <Link to={`/profiles/${activity.hostId}`}>
+                {activity.hostDisplayName}
+              </Link>
             </>
           }
         />
         <Box display="flex" flexDirection="column" gap={2} mr={2}>
-          {(isHost || isGoing) && (
-            <Chip label={label} color={color} sx={{ borderRadius: 2 }} />
+          {(activity.isHost || activity.isGoing) && (
+            <Chip
+              variant="outlined"
+              label={label}
+              color={color}
+              sx={{ borderRadius: 2 }}
+            />
           )}
-          {isCancelled && (
+          {activity.isCancelled && (
             <Chip label="Cancelled" color="error" sx={{ borderRadius: 2 }} />
           )}
         </Box>
@@ -71,7 +81,9 @@ export default function ActivityCard({ activity }: Props) {
           gap={2}
           sx={{ backgroundColor: "grey.200", py: 3, pl: 3 }}
         >
-          Attendees go here
+          {activity.attendees.map((att) => (
+            <AvatarPopover profile={att} key={att.id} />
+          ))}
         </Box>
       </CardContent>
 
