5115d431dba5ca5514bea2e4b97d6f27fd5d555f
NuwanSD <sagaradnuwan@gmail.com>
2025-06-10 00:53:48 +0530

    end of section 19

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -29,4 +29,5 @@ public class Activity
 
     //navigation properties
     public ICollection<ActivityAttendee> Attendees { get; set; } = [];
+    public ICollection<Comment> Comments { get; set; } = [];
 }
--- client/src/lib/util/util.ts
+++ client/src/lib/util/util.ts
@@ -1,5 +1,12 @@
-import { format, type DateArg } from "date-fns";
+import { format, formatDistanceToNow, isValid, type DateArg } from "date-fns";
 
 export function formatDate(date: DateArg<Date>) {
   return format(date, "dd MMM yyyy h:mm a");
+}
+
+export function timeAgo(date: DateArg<Date> | string) {
+  const dateObj = typeof date === "string" ? new Date(date) : date;
+  if (!isValid(dateObj)) return "Invalid date";
+
+  return formatDistanceToNow(dateObj) + " ago";
 }
